0151c376a41e3ad3d9ba8220596f4c549401e394
Jamey Kirk <jameykirk@gmail.com>
2021-03-05 12:15:26 +0900

    Job Done

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function drawTriangle(size) {
 
    // Your solution goes here
-   
+   let count = 0;
+   let chrStr = ""
+   while (size > 0) {
+      count++;
+      while (count > 0) {
+         chrStr = chrStr + "*";
+         count--;
+      }
+      count = chrStr.length;
+      console.log(chrStr);
+      chrStr = "";
+      size--;
+   }
 }
+
+drawTriangle(10);
